3bec2d8ae3b15b6fc54e4d3b1bf9357f22782ee0
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-27 17:39:56 -0500

    add sliding functionality!

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -14,12 +14,29 @@ export function Board() {
 
   const [numbers, setNumbers] = useState(shuffle());
 
+  const moveTile = (tile) => {
+    const i16 = numbers.find((n) => n.value === 16).index;
+    if (![i16 - 1, i16 + 1, i16 - 4, i16 + 4].includes(tile.index)) {
+      return;
+    }
+
+    const newNumbers = [...numbers].map((number) => {
+      if (number.index !== i16 && number.index !== tile.index) {
+        return number;
+      } else if (number.value === 16) {
+        return { value: 16, index: tile.index };
+      }
+      return { value: tile.value, index: i16 };
+    });
+    setNumbers(newNumbers);
+  };
+
   return (
     <div className="game">
       <div className="board">
         <Overlay />
         {numbers.map((x, index) => (
-          <Tile key={index} number={x} />
+          <Tile key={index} number={x} moveTile={moveTile} />
         ))}
       </div>
     </div>
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -1,13 +1,14 @@
 import "./Tile.css";
 
-export function Tile({ number }) {
+export function Tile({ number, moveTile }) {
   return (
     <div
+      onClick={() => moveTile(number)}
       className={`number ${number.value === 16 ? "disabled" : ""} slot--${
         number.index
       }`}
     >
-      {number.value}
+      {number.value === 16 ? "" : number.value}
     </div>
   );
 }
