a226d65c988e8945033e67cdae692e39433c05d7
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-27 17:15:35 -0500

    show all tiles

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -2,7 +2,11 @@ import "./Tile.css";
 
 export function Tile({ number }) {
   return (
-    <div className={`number ${number.value === 16 ? "disabled" : ""}`}>
+    <div
+      className={`number ${number.value === 16 ? "disabled" : ""} slot--${
+        number.index
+      }`}
+    >
       {number.value}
     </div>
   );
